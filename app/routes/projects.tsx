fe2a09b9ba2dd87159826c930560295aa6977480
Ala Ben Aissia <xalabenaissiax@outlook.com>
2025-09-28 16:47:28 +0100

    projects category filter

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -2,6 +2,7 @@ import ProjectCard from "~/components/ProjectCard";
 import type { Route } from "./+types/projects";
 import Pagination from "~/components/Pagination";
 import { usePage } from "~/hooks/usePage";
+import { useState } from "react";
 
 type Project = {
   id: number;
@@ -22,26 +23,55 @@ export async function loader() {
 }
 
 export default function ProjectsPage({ loaderData }: Route.ComponentProps) {
+  const [category, setCategory] = useState("All");
+  const projects = loaderData;
+
+  const filteredProjects =
+    category === "All"
+      ? projects
+      : projects.filter((p) => p.category === category);
+
   const {
-    items: projects,
+    items: displayedProject,
     totalPages,
     currentPage,
     onPageChange,
     goNext,
     goPrev,
   } = usePage({
-    list: loaderData,
+    list: filteredProjects,
     perPage: 4,
   });
+
+  const categories = ["All", ...new Set(projects.map((p) => p.category))];
 
   return (
     <>
       <h2 className="text-2xl sm:text-3xl lg:text-4xl text-white font-bold mb-8 bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text">
         ✨ My Projects
       </h2>
-
+      <div className="flex flex-wrap gap-2 mb-8">
+        {categories.map((c) => (
+          <button
+            onClick={() => {
+              onPageChange(1);
+              setCategory(c);
+            }}
+            name="category"
+            id="categoy"
+            className={`px-3 py-1 text-sm rounded cursor-pointer ${
+              category === c
+                ? "bg-blue-600 text-white"
+                : "bg-gray-700 text-gray-200"
+            }`}
+            key={c}
+          >
+            {c}
+          </button>
+        ))}
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {projects.map((project) => (
+        {displayedProject.map((project) => (
           <ProjectCard project={project} key={project.id} />
         ))}
       </div>
