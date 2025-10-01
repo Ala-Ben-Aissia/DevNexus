ab550cb6daa33b4076dd1d80e3ed4f474971960b
Ala Ben Aissia <xalabenaissiax@outlook.com>
2025-10-01 11:24:05 +0100

    set up strapi

--- app/routes/layouts/main.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router";
-
-export default function MainLayout() {
-  return (
-    <section className="w-full max-w-6xl mx-auto px-4 my-6">
-      <Outlet />
-    </section>
-  );
-}
