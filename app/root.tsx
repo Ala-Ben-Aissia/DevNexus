60b27d31e87c5e29e2fd32fa3d06140e3bc4d030
Ala Ben Aissia <xalabenaissiax@outlook.com>
2025-11-07 12:19:24 +0100

    serve favicon logo

--- app/root.tsx
+++ app/root.tsx
@@ -5,27 +5,20 @@ import {
 	Outlet,
 	Scripts,
 	ScrollRestoration,
+	type LinksFunction,
 } from 'react-router'
 
 import {Analytics} from '@vercel/analytics/react'
 import {SpeedInsights} from '@vercel/speed-insights/react'
 import type {Route} from './+types/root'
 import './app.css'
+import faviconAssetUrl from './assets/favicon.svg'
 import Navbar from './components/Navbar'
 
 import {ThemeProvider} from './contexts/ThemeContext'
 
-export const links: Route.LinksFunction = () => [
-	{rel: 'preconnect', href: 'https://fonts.googleapis.com'},
-	{
-		rel: 'preconnect',
-		href: 'https://fonts.gstatic.com',
-		crossOrigin: 'anonymous',
-	},
-	{
-		rel: 'stylesheet',
-		href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
-	},
+export const links: LinksFunction = () => [
+	{rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl},
 ]
 
 export function meta({}: Route.MetaArgs) {
